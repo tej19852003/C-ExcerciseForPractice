9e538751112ded4f7d43cfe66a6070aaa1993ec8
Tej Patel <tej19852003@gmail.com>
2018-01-05 10:50:13 -0600

    NUnitTestPrject for C# practice
    NUnitTestPrject for C# practice

---
+++ NUNITTestProject/NUNITTestProject/ExtentReportTest.cs
@@ -0,0 +1,82 @@
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using RelevantCodes.ExtentReports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUNITTestProject
+{
+    [TestFixture]
+    public class ExtentReportTest
+    {
+        ExtentReports extent = new ExtentReports("C:\\CSharp\\report\\report.html");
+        IWebDriver driver = null;
+        private ExtentTest test;
+
+        [OneTimeSetUp]
+        public void OnetimeSetUpTest()
+        {
+            extent = new ExtentReports("C:\\CSharp\\report\\report.html");
+        }
+        
+
+        [Test]
+        public void StartDriver()
+        {
+            test = extent.StartTest("Start Driver Test", "Starting Driver and Validating Download link");
+
+
+            driver = new ChromeDriver(@"C:\\CSharp\\NUNITTestProject\\NUNITTestProject\\Files");
+            test.Log(LogStatus.Info, "Starting Chrome Driver");
+            driver.Url = "http://www.seleniumhq.org/";
+            test.Log(LogStatus.Info, "Navigating to seleniumhq.org website");
+
+            driver.Manage().Window.Maximize();
+            test.Log(LogStatus.Info, "Maximized the window");
+
+            bool check = driver.FindElement(By.LinkText("Download")).Displayed;
+            test.Log(LogStatus.Pass, "Verified the download link is present");
+
+
+            Console.WriteLine("check = " + check);
+
+           
+        }
+        [Test]
+        public void VerifyChromeDriverVersion()
+        {
+            test = extent.StartTest("VerifyChromeDriverVersion", "Verify ChromeDriver latest Version");
+
+            driver.FindElement(By.LinkText("Download")).Click();
+            test.Log(LogStatus.Pass, "Clicked on Download Link");
+
+            String currenturl = driver.Url;
+            if (currenturl.Contains("seleniumhq.org/download/")){
+                test.Log(LogStatus.Pass, "Verified Page navigated to right URL");
+                Assert.IsTrue(true);
+
+            } else
+            {
+                test.Log(LogStatus.Fail, "Verified Page navigated to wrong URL");
+                Console.WriteLine("Current URL = " + currenturl);
+                Assert.IsTrue(false);
+            }
+            
+
+
+        }
+        [OneTimeTearDown]
+        public void OnetimeTearDownTest()
+        {
+            extent.EndTest(test);
+            extent.Flush();
+
+            driver.Quit();
+        }
+    }
+}
---
+++ NUNITTestProject/NUNITTestProject/MyTestClass3.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUNITTestProject
+{
+    [TestFixture]
+    public class MyTestClass5
+    {
+        [Test, TestCaseSource("DataSuppy")]
+        public void TestMethod5(String username, String password, String email, String city)
+        {
+            Console.WriteLine(username + "--" + password + "--" + email + "--" + city );
+        }
+
+        public static object[] DataSuppy()
+        {
+            object[][] data = new object[4][];
+
+            data[0] = new object[4];
+            data[0][0] = "user0";
+            data[0][1] = "pass0";
+            data[0][2] = "email0";
+            data[0][3] = "city0";
+
+            data[1] = new object[4];
+            data[1][0] = "user1";
+            data[1][1] = "pass1";
+            data[1][2] = "email1";
+            data[1][3] = "city1";
+
+            data[2] = new object[4];
+            data[2][0] = "user2";
+            data[2][1] = "pass2";
+            data[2][2] = "email2";
+            data[2][3] = "city2";
+
+            //data[3] = new object[4];
+            //data[3][0] = "user3";
+            //data[3][1] = "pass3";
+            //data[3][2] = "email3";
+            //data[3][3] = "city3";
+
+            return data;
+        }
+    }
+}
